e865a73098f70cd4588f889677b044c191b64e53
Kseniia Tikhonova <tikhonovakb@gmail.com>
2023-07-11 17:07:43 +0500

    feat: cors

--- backend/app.js
+++ backend/app.js
@@ -5,18 +5,65 @@ const { errors } = require('celebrate');
 const router = require('./routes');
 const auth = require('./middlewares/auth');
 const errorHandler = require('./middlewares/errorHandler');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+
+const { PORT = 3000 } = process.env;
 
 const NotFoundError = require('./errors/not-found-err');
-
-// импорт роутов signin и signup
-const singInRoutes = require('./routes/signin');
-const singUpRoutes = require('./routes/signup');
 
 const app = express();
 
 // подключаемся к серверу mongo
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
   useNewUrlParser: true,
+});
+
+app.use(requestLogger); // подключаем логгер запросов
+
+// импорт роутов signin и signup
+const singInRoutes = require('./routes/signin');
+const singUpRoutes = require('./routes/signup');
+
+// обработка CORS запросов:
+// Массив доменов, с которых разрешены кросс-доменные запросы
+const allowedCors = [
+  'https://kseniia-mesto.nomoreparties.sbs',
+  'http://ksenia-mesto.nomoreparties.sbs',
+  'http://localhost:3000',
+  'http://localhost:3001',
+];
+
+app.use((req, res, next) => {
+  const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
+  // проверяем, что источник запроса есть среди разрешённых
+  if (allowedCors.includes(origin)) {
+    // устанавливаем заголовок, который разрешает браузеру запросы с этого источника
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+
+  const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
+  // Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  // сохраняем список заголовков исходного запроса
+  const requestHeaders = req.headers['access-control-request-headers'];
+  // Если это предварительный запрос, добавляем нужные заголовки
+  if (method === 'OPTIONS') {
+    // разрешаем кросс-доменные запросы любых типов (по умолчанию)
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    // разрешаем кросс-доменные запросы заголовков исходного запроса
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    // завершаем обработку запроса и возвращаем результат клиенту
+    return res.end();
+  }
+
+  return next();
+});
+
+// Краш-тест сервера
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
 });
 
 // устанавливает middleware для парсинга JSON-тела запросов
@@ -34,10 +81,11 @@ app.use(router);
 
 app.use((req, res, next) => next(new NotFoundError('Route not found')));
 
+app.use(errorLogger); // подключаем логгер ошибок
 app.use(errors()); // обработчик ошибок celebrate
 
 app.use(errorHandler); // централизованный обработчик
 
-app.listen(3000, () => {
-  console.log('I am listening port 3000');
+app.listen(PORT, () => {
+  console.log(`I am listening port ${PORT}`);
 });
