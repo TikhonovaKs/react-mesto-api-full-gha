4933928db755d64868cb9b29c03efdbb8080d86e
Kseniia Tikhonova <tikhonovakb@gmail.com>
2023-07-14 22:23:08 +0500

    removed http only

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -49,7 +49,7 @@ const login = (req, res, next) => {
           const jwt = jsonWebToken.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret', { expiresIn: '1w' });
           res.cookie('jwt', jwt, {
             maxAge: 36000,
-            httpOnly: false,
+            // httpOnly: false,
             sameSite: true,
           });
           res.send(user.toJSON());
